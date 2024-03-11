ef45e572d1bec0524489376cb7c77df4d732a92c
Laszlo Prokaj <prokaj.laszlo96@gmail.com>
2024-03-11 11:52:02 +0100

    add GeocodingApiProvider

---
+++ SolarWatch/Service/Geocoding/GeocodingApiProvider.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+
+namespace SolarWatch.Service.Geocoding;
+
+public class GeocodingApiProvider : IGeocodingApiProvider
+{
+    private readonly ILogger<GeocodingApiProvider> _logger;
+
+    public GeocodingApiProvider(ILogger<GeocodingApiProvider> logger)
+    {
+        _logger = logger;
+    }
+    public string GetCityCoordinates(string city)
+    {
+        var apiKey = "ba0dd16bccd1e4d44407f12d6caf35da";
+        var url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}";
+
+        using var client = new WebClient();
+        
+        _logger.LogInformation("Calling OpenWeather API with url: {url}", url);
+        return client.DownloadString(url);
+    }
+}
---
+++ SolarWatch/Service/Geocoding/IGeocodingApiProvider.cs
@@ -0,0 +1,6 @@
+﻿namespace SolarWatch.Service.Geocoding;
+
+public interface IGeocodingApiProvider
+{
+    string GetCityCoordinates(string city);
+}
