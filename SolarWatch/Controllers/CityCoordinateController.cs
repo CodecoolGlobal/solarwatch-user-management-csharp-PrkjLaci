3ebeb5212832d83eb31cfc344a51eda17e75f241
Laszlo Prokaj <prokaj.laszlo96@gmail.com>
2024-03-11 12:03:20 +0100

    add CityCoordinateController

---
+++ SolarWatch/Controllers/CityCoordinateController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SolarWatch.Service.Geocoding;
+
+namespace SolarWatch.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CityCoordinateController : ControllerBase
+{
+    private readonly ILogger<CityCoordinateController> _logger;
+    private readonly IGeocodingApiProvider _geocodingApiProvider;
+    
+    public CityCoordinateController(ILogger<CityCoordinateController> logger, IGeocodingApiProvider geocodingApiProvider)
+    {
+        _logger = logger;
+        _geocodingApiProvider = geocodingApiProvider;
+    }
+    
+    [HttpGet("GetCityCoordinates")]
+    public IActionResult GetCityCoordinates(string city)
+    {
+        try
+        {
+            var cityCoordinates = _geocodingApiProvider.GetCityCoordinates(city);
+            _logger.LogInformation("City coordinates: {cityCoordinates}", cityCoordinates);
+            return Ok(cityCoordinates);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error getting city coordinates");
+            return BadRequest("Error getting city coordinates");
+        }
+    }
+}
