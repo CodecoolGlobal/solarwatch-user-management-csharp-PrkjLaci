1f7f6e49f6806f7ff6b607de00ba45cc7b2b78bd
Laszlo Prokaj <prokaj.laszlo96@gmail.com>
2024-03-11 17:32:22 +0100

    relationship between CityData and SunsetSunriseTime implemented

--- SolarWatch/Models/CityData.cs
+++ SolarWatch/Models/CityData.cs
@@ -1,10 +1,17 @@
-﻿namespace SolarWatch.Models;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SolarWatch.Models;
 
 public class CityData
 {
+    public int Id { get; set; }
     public string? CityName { get; set; }
     public double Latitude { get; set; }
     public double Longitude { get; set; }
     public string? State { get; set; }
     public string? Country { get; set; }
+    
+    [ForeignKey("SunsetSunriseTime")]
+    public int SunsetSunriseTimeId { get; set; }
+    public SunsetSunriseTime? SunsetSunriseTime { get; set; }
 }
