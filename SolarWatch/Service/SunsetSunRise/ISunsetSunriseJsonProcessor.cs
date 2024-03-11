d9e378f8cd3c967afdb75f0f4e8a114acc17ec64
Laszlo Prokaj <prokaj.laszlo96@gmail.com>
2024-03-11 12:51:36 +0100

    add SunsetSunriseJsonProcessor

---
+++ SolarWatch/Service/SunsetSunRise/ISunsetSunriseJsonProcessor.cs
@@ -0,0 +1,8 @@
+﻿using SolarWatch.Models;
+
+namespace SolarWatch.Service.SunsetSunRise;
+
+public interface ISunsetSunriseJsonProcessor
+{
+    SunsetSunriseTime Process(string data);
+}
---
+++ SolarWatch/Service/SunsetSunRise/SunsetSunriseJsonProcessor.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+using SolarWatch.Models;
+
+namespace SolarWatch.Service.SunsetSunRise;
+
+public class SunsetSunriseJsonProcessor : ISunsetSunriseJsonProcessor
+{
+    public SunsetSunriseTime Process(string data)
+    {
+        JsonDocument json = JsonDocument.Parse(data);
+        JsonElement root = json.RootElement;
+
+        var results = root.GetProperty("results");
+
+        SunsetSunriseTime sunsetSunriseTime = new SunsetSunriseTime
+        {
+            Sunrise = results.GetProperty("sunrise").ToString(),
+            Sunset = results.GetProperty("sunset").ToString()
+        };
+
+        return sunsetSunriseTime;
+    }
+}
