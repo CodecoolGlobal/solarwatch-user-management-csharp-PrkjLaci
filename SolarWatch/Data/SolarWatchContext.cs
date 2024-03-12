7c40b3352eec11fa96ccb451da456ecfff72ce77
Laszlo Prokaj <prokaj.laszlo96@gmail.com>
2024-03-12 01:48:44 +0100

    implemented one-to-one relation between City and SunsetSunrise

--- SolarWatch/Data/SolarWatchContext.cs
+++ SolarWatch/Data/SolarWatchContext.cs
@@ -5,7 +5,7 @@ namespace SolarWatch.Data;
 
 public class SolarWatchContext : DbContext
 {
-    public DbSet<CityData> CityData { get; set; }
+    public DbSet<City> CityData { get; set; }
     public DbSet<SunsetSunriseTime> SunsetSunriseTime { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -17,10 +17,14 @@ public class SolarWatchContext : DbContext
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<CityData>()
+        modelBuilder.Entity<City>()
+            .HasIndex(u => u.State)
+            .IsUnique();
+        
+        modelBuilder.Entity<City>()
             .HasOne(c => c.SunsetSunriseTime)
-            .WithOne()
-            .HasForeignKey<CityData>(c => c.SunsetSunriseTimeId);
+            .WithOne(s => s.City)
+            .HasForeignKey<SunsetSunriseTime>(s => s.Id);
     }
     
 }
