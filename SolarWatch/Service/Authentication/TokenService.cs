accd92a2a579c75174f8615b5b29756885df02e4
Laszlo Prokaj <prokaj.laszlo96@gmail.com>
2024-03-13 23:50:57 +0100

    add TokenService

---
+++ SolarWatch/Service/Authentication/TokenService.cs
@@ -0,0 +1,68 @@
+﻿using System.Globalization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+
+namespace SolarWatch.Service.Authentication;
+
+public class TokenService : ITokenService
+{
+    private const int ExpirationMinutes = 30;
+    private readonly IConfiguration _configuration;
+    
+    public string CreateToken(IdentityUser user)
+    {
+        var expiration = DateTime.UtcNow.AddMinutes(ExpirationMinutes);
+        var token = CreateJwtToken(
+            CreateClaims(user),
+            CreateSigningCredentials(),
+            expiration
+            );
+        var tokenHandler = new JwtSecurityTokenHandler();
+        return tokenHandler.WriteToken(token); 
+    }
+
+    private JwtSecurityToken CreateJwtToken(List<Claim> claims, SigningCredentials credentials,
+        DateTime expiration) =>
+        new(
+            "MySolarWatchApp",
+            "MySolarWatchApp",
+            claims,
+            expires: expiration,
+            signingCredentials: credentials
+        );
+
+    private List<Claim> CreateClaims(IdentityUser user)
+    {
+        try
+        {
+            var claims = new List<Claim>
+            {
+                new(JwtRegisteredClaimNames.Sub, "TokenForTheApiWithAuth"),
+                new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString(CultureInfo.InvariantCulture)),
+                new(ClaimTypes.NameIdentifier, user.Id),
+                new(ClaimTypes.Name, user.UserName),
+                new(ClaimTypes.Email, user.Email)
+            };
+            return claims;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    private SigningCredentials CreateSigningCredentials()
+    {
+        return new SigningCredentials(
+            new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration["JwtOptions:IssuerSigningKey"])
+            ),
+            SecurityAlgorithms.HmacSha256
+        );
+    }
+}
