19d3a1d6a97aa0b62ae6cde83e00d786c721ea61
Laszlo Prokaj <prokaj.laszlo96@gmail.com>
2024-03-13 21:15:56 +0100

    add Swagger configuration

--- SolarWatch/Program.cs
+++ SolarWatch/Program.cs
@@ -49,11 +49,33 @@ void AddServices()
 
 void ConfigureSwagger()
 {
-    if (app.Environment.IsDevelopment())
+    builder.Services.AddSwaggerGen(option =>
     {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
+        option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+        option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Description = "Please enter a valid token",
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            BearerFormat = "JWT",
+            Scheme = "Bearer"
+        });
+        option.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                new string[]{}
+            }
+        });
+    });
 }
 
 void AddDbContext()
