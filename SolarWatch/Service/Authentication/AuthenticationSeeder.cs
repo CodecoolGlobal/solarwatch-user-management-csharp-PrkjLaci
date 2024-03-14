813512136ded1315ac7dc125b6aefe9f5899ad56
Laszlo Prokaj <prokaj.laszlo96@gmail.com>
2024-03-14 13:14:43 +0100

    add method CreateAdminIfNotExists ti AuthenticationSeeder

--- SolarWatch/Service/Authentication/AuthenticationSeeder.cs
+++ SolarWatch/Service/Authentication/AuthenticationSeeder.cs
@@ -5,11 +5,13 @@ namespace SolarWatch.Service.Authentication;
 public class AuthenticationSeeder
 {
     private RoleManager<IdentityRole> roleManager;
+    private UserManager<IdentityUser> userManager;
     private IConfiguration configuration;
 
-    public AuthenticationSeeder(RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+    public AuthenticationSeeder(RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager, IConfiguration configuration)
     {
         this.roleManager = roleManager;
+        this.userManager = userManager;
         this.configuration = configuration;
     }
 
@@ -30,5 +32,20 @@ public class AuthenticationSeeder
     private async Task CreateUserRole(RoleManager<IdentityRole> roleManager)
     {
         await roleManager.CreateAsync(new IdentityRole(configuration["Roles:UserRoleName"]));
+    }
+    
+    private async Task CreateAdminIfNotExists()
+    {
+        var adminInDb = await userManager.FindByEmailAsync("admin@admin.com");
+        if (adminInDb == null)
+        {
+            var admin = new IdentityUser { UserName = "admin", Email = "admin@admin.com" };
+            var adminCreated = await userManager.CreateAsync(admin, "admin123");
+
+            if (adminCreated.Succeeded)
+            {
+                await userManager.AddToRoleAsync(admin, "Admin");
+            }
+        }
     }
 }
