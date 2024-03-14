39eddf8f223597f5ed91c9890d2101399b14a6a0
Laszlo Prokaj <prokaj.laszlo96@gmail.com>
2024-03-14 12:37:23 +0100

    add some minor changes

--- SolarWatch/Service/Authentication/AuthenticationSeeder.cs
+++ SolarWatch/Service/Authentication/AuthenticationSeeder.cs
@@ -4,31 +4,31 @@ namespace SolarWatch.Service.Authentication;
 
 public class AuthenticationSeeder
 {
-    private readonly RoleManager<IdentityRole> _roleManager;
-    private readonly IConfiguration _configuration;
+    private RoleManager<IdentityRole> roleManager;
+    private IConfiguration configuration;
 
     public AuthenticationSeeder(RoleManager<IdentityRole> roleManager, IConfiguration configuration)
     {
-        _roleManager = roleManager;
-        _configuration = configuration;
+        this.roleManager = roleManager;
+        this.configuration = configuration;
     }
 
     public void AddRoles()
     {
-        var tAdmin = CreateAdminRole(_roleManager);
+        var tAdmin = CreateAdminRole(roleManager);
         tAdmin.Wait();
         
-        var tUser = CreateUserRole(_roleManager);
+        var tUser = CreateUserRole(roleManager);
         tUser.Wait();
     }
 
     private async Task CreateAdminRole(RoleManager<IdentityRole> roleManager)
     {
-        await roleManager.CreateAsync(new IdentityRole(_configuration["Roles:AdminRoleName"]));
+        await roleManager.CreateAsync(new IdentityRole(configuration["Roles:AdminRoleName"]));
     }
 
     private async Task CreateUserRole(RoleManager<IdentityRole> roleManager)
     {
-        await roleManager.CreateAsync(new IdentityRole(_configuration["Roles:UserRoleName"]));
+        await roleManager.CreateAsync(new IdentityRole(configuration["Roles:UserRoleName"]));
     }
 }
