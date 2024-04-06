2c362fddaead17e67de0b57e9bb58dddee4a4d0b
Laszlo Prokaj <prokaj.laszlo96@gmail.com>
2024-04-06 19:40:28 +0200

    modified AuthResult, added Role

--- SolarWatch/Service/Authentication/AuthService.cs
+++ SolarWatch/Service/Authentication/AuthService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using SolarWatch.Contracts;
 
 namespace SolarWatch.Service.Authentication;
 
@@ -24,7 +25,7 @@ public class AuthService : IAuthService
         }
 
         await _userManager.AddToRoleAsync(user, role);
-        return new AuthResult(true, email, username, "");
+        return new AuthResult(true, email, username, "", role);
     }
 
     public async Task<AuthResult> LoginAsync(string email, string password)
@@ -44,16 +45,22 @@ public class AuthService : IAuthService
         }
 
         var roles = await _userManager.GetRolesAsync(managedUser);
-        Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-        Console.WriteLine(roles[0]);
         var accessToken = _tokenService.CreateToken(managedUser, roles[0]);
 
-        return new AuthResult(true, managedUser.Email, managedUser.UserName, accessToken);
+        return new AuthResult(true, managedUser.Email, managedUser.UserName, accessToken, roles[0]);
     }
+    
+    public async Task<UserResponse> GetUserInformation(string email)
+    {
+        var user = await _userManager.FindByEmailAsync(email);
+        var roles = await _userManager.GetRolesAsync(user);
+        return new UserResponse(user.Id, user.Email, user.UserName, user.PhoneNumber,roles[0]);
+    }
+    
 
     private static AuthResult FailedRegistration(IdentityResult result, string email, string username)
     {
-        var authResult = new AuthResult(false, email, username, "");
+        var authResult = new AuthResult(false, email, username, "", "");
 
         foreach (var error in result.Errors)
         {
@@ -65,14 +72,14 @@ public class AuthService : IAuthService
 
     private static AuthResult InvalidEmail(string email)
     {
-        var result = new AuthResult(false, email, "", "");
+        var result = new AuthResult(false, email, "", "", "");
         result.ErrorMessages.Add("Bad credentials", "Invalid email");
         return result;
     }
 
     private static AuthResult InvalidPassword(string email, string userName)
     {
-        var result = new AuthResult(false, email, userName, "");
+        var result = new AuthResult(false, email, userName, "", "");
         result.ErrorMessages.Add("Bad credentials", "Invalid password");
         return result;
     }
