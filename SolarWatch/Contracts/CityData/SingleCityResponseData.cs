9b0c821909bef10ad72ce17f1aa375ce12145360
Laszlo Prokaj <prokaj.laszlo96@gmail.com>
2024-04-11 14:44:13 +0200

    add Response classes for integration test

---
+++ SolarWatch/Contracts/CityData/SingleCityResponseData.cs
@@ -0,0 +1,10 @@
+﻿using SolarWatch.Models;
+
+namespace SolarWatch.Contracts.CityData;
+
+public class SingleCityResponseData
+{
+    public int Id { get; set; }
+    public string Message { get; set; }
+    public City Data { get; set; }
+}
---
+++ SolarWatch/Contracts/SunsetSunrise/SunsetSunriseResponseData.cs
@@ -0,0 +1,10 @@
+﻿using SolarWatch.Models;
+
+namespace SolarWatch.Contracts.SunsetSunrise;
+
+public class SunsetSunriseResponseData
+{
+    public int Id { get; set; }
+    public string Message { get; set; }
+    public List<SunsetSunriseTime> Data { get; set; }
+}
