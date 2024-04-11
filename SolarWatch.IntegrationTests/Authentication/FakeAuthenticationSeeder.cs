a6527649806c124012f37ef1c5b3ce59e67dd7ae
Laszlo Prokaj <prokaj.laszlo96@gmail.com>
2024-04-11 14:59:03 +0200

    add helper methods for authentication in integration tests

---
+++ SolarWatch.IntegrationTests/Authentication/FakeAuthenticationSeeder.cs
@@ -0,0 +1,17 @@
+﻿using SolarWatch.Service.Authentication;
+
+namespace SolarWatch.IntegrationTests.Authentication;
+
+public class FakeAuthenticationSeeder : IAuthenticationSeeder
+{
+    public void AddRoles()
+    {
+        Console.WriteLine("Adding roles");
+        return;
+    }
+
+    public void AddAdmin()
+    {
+        return;
+    }
+}
---
+++ SolarWatch.IntegrationTests/Authentication/TestJwtToken.cs
@@ -0,0 +1,34 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace SolarWatch.IntegrationTests.Authentication;
+
+public class TestJwtToken
+{
+    private List<Claim> Claims { get; } = new();
+    private int ExpiresInMinutes { get; set; } = 30;
+
+    public TestJwtToken WithRole(string roleName)
+    {
+        Claims.Add(new Claim(ClaimTypes.Role, roleName));
+        return this;
+    }
+    
+    public TestJwtToken WithName(string username)
+    {
+        Claims.Add(new Claim(ClaimTypes.Name, username));
+        return this;
+    }
+
+    public string Build()
+    {
+        var token = new JwtSecurityToken(
+            JwtTokenHelper.Issuer,
+            JwtTokenHelper.Issuer,
+            Claims,
+            expires: DateTime.Now.AddMinutes(ExpiresInMinutes),
+            signingCredentials: JwtTokenHelper.SigningCredentials
+        );
+        return JwtTokenHelper.JwtSecurityTokenHandler.WriteToken(token);
+    }
+}
