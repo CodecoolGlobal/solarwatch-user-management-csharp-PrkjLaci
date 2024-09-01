086886eba3e90d31ff3484d27388471fb1f3217d
PrkjLaci <prokaj.laszlo96@gmail.com>
2024-09-01 19:29:51 +0200

    Remove unnecessary whitespace

--- SolarWatch/Repository/SunsetSunriseRepository/SunsetSunriseRepository.cs
+++ SolarWatch/Repository/SunsetSunriseRepository/SunsetSunriseRepository.cs
@@ -28,7 +28,6 @@ public class SunsetSunriseRepository : ISunsetSunriseRepository
     {
         return await _dbContext.SunsetSunriseTime.ToListAsync();
     }
-
     public async Task<SunsetSunriseTime> AddSunsetSunrise(SunsetSunriseTime sunsetSunrise)
     {
         var city = await _dbContext.CityData.Include(city => city.SunsetSunriseTime).FirstOrDefaultAsync(c => c.Id == sunsetSunrise.CityId);
@@ -111,6 +110,4 @@ public class SunsetSunriseRepository : ISunsetSunriseRepository
         
         await _dbContext.SaveChangesAsync();
     }
-
-
 }
